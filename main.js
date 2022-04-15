610c8c8400db70814b89d26843a64fdd6ebe0d3d
Shakzod Muradov <shaxzod1996shax@gmail.com>
2022-04-15 08:10:06 +0500

    dfg

--- main.js
+++ main.js
@@ -1,57 +1,245 @@
 let placeContainer = document.querySelector(".place-container");
-let id = 0;
-for (let i = 1; i <= 8; i++) {
-  let elBoardBorder = document.createElement("div");
-  elBoardBorder.classList.add("d-flex");
-  elBoardBorder.classList.add("rows");
-  placeContainer.appendChild(elBoardBorder);
-  if (i % 2 == 1) {
-  }
-  for (let j = 1; j <= 8; j++) {
-    id++;
-    let elBoardPlace = document.createElement("div");
-    elBoardPlace.id = id;
-    elBoardPlace.classList.add("card");
-    elBoardBorder.appendChild(elBoardPlace);
-  }
-  console.log(elBoardBorder);
+
+for (let card of arr) {
+  if (card.y % 2 == 0 && card.x % 2 == 0) {
+    card.color = "dark";
+  } 
+  if (card.y % 2 != 0 && card.x % 2 != 0) {
+    card.color = "dark";
+  } 
 }
+
+for (let i of arr) {
+  let card = document.createElement("div");
+  card.classList.add(`y_${i.y}`, `x_${i.x}`, `card`, `${i.color}_color`);
+  placeContainer.appendChild(card);
+}
+// ?Click to the stones
+let stons = document.querySelectorAll(".stone");
+
+let newStone = "";
+stons.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (e.target.classList.contains("king")) {
+      newStone = "king";
+    }
+
+    if (e.target.classList.contains("queen")) {
+      newStone = "queen";
+    }
+
+    if (e.target.classList.contains("castle")) {
+      newStone = "castle";
+    }
+
+    if (e.target.classList.contains("bishop")) {
+      newStone = "bishop";
+    }
+
+    if (e.target.classList.contains("knight")) {
+      newStone = "knight";
+    }
+
+    if (e.target.classList.contains("pawl")) {
+      newStone = "pawl";
+    }
+
+    stons.forEach((el) => {
+      el.classList.remove("tosh-fixed-active");
+    });
+
+    e.target.classList.add("tosh-fixed-active");
+  });
+});
 
 let places = placeContainer.querySelectorAll(".card");
 
-arr.forEach((element, index) => {
-  places.forEach((item) => {
-    item.addEventListener("mouseover", (e) => {
-      if (item.id == element.id) {
-        let x = document.querySelector(".x");
-        let y = document.querySelector(".y");
-        x.innerHTML = `= ${element.x}`;
-        y.innerHTML = `= ${element.y}`;
-        item.classList.add('ston_selected')
+for (let card of places) {
+  card.addEventListener("mouseover", (e) => {
+    let x = Number(e.target.classList[1].slice(-1));
+    let y = Number(e.target.classList[0].slice(-1));
+
+    let x1 = document.querySelector(".x");
+    let y1 = document.querySelector(".y");
+
+    x1.innerHTML = `x = ${x}`;
+    y1.innerHTML = `y = ${y}`;
+
+    for (let card of places) {
+      card.classList.remove("help");
+    }
+
+    if (newStone == "king") {
+      for (let card of places) {
+        console.log(card);
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+        }
       }
-    });
+    }
 
-    item.addEventListener("mouseout", (e) => {
-      if (item.id == element.id) {
-        let x = document.querySelector(".x");
-        let y = document.querySelector(".y");
-        x.innerHTML = "";
-        y.innerHTML = "";
-        item.classList.remove('ston_selected')
+    if (newStone == "queen") {
+      for (let card of places) {
+        for (let i = 8; i >= 1; i--) {
+          card.classList.contains(`y_${y}`) && card.classList.contains(`x_${i}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+        }
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${i}`) && card.classList.contains(`x_${x}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+        }
       }
-    });
+    }
+
+    if (newStone == "castle") {
+      for (let card of places) {
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${i}`) && card.classList.contains(`x_${x}`)
+            ? card.classList.add("help")
+            : '';
+        }
+        for (let i = 8; i >= 1; i--) {
+          card.classList.contains(`y_${y}`) && card.classList.contains(`x_${i}`)
+            ? card.classList.add("help")
+            : '';
+        }
+      }
+    }
+
+    if (newStone == "knight") {
+      for (let card of places) {
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x + 2}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 2}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 2}`) &&
+          card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + 1}`) &&
+          card.classList.contains(`x_${x - 2}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x + 2}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x - 2}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 2}`) &&
+            card.classList.contains(`x_${x - 1}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - 2}`) &&
+          card.classList.contains(`x_${x + 1}`)
+            ? card.classList.add("help")
+            : '';
+        }
+      }
+    }
+
+    if (newStone == "bishop") {
+      for (let card of places) {
+        for (let i = 8; i >= 1; i--) {
+          card.classList.contains(`y_${y - i}`) &&
+          card.classList.contains(`x_${x - i}`)
+            ? card.classList.add("help")
+            : '';
+        }
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${y + i}`) &&
+          card.classList.contains(`x_${x + i}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y - i}`) &&
+          card.classList.contains(`x_${x + i}`)
+            ? card.classList.add("help")
+            : '';
+          card.classList.contains(`y_${y + i}`) &&
+          card.classList.contains(`x_${x - i}`)
+            ? card.classList.add("help")
+            : '';
+        }
+      }
+    }
+
+    if (newStone == "pawl") {
+      for (let card of places) {
+        for (let i = 1; i <= 8; i++) {
+          card.classList.contains(`y_${y - 1}`) &&
+          card.classList.contains(`x_${x}`)
+            ? card.classList.add("help")
+            : '';
+          y == 8 ? card.classList.remove("help") : '';
+        }
+      }
+    }
   });
-});
 
-// ?Click to the stones
+  placeContainer.addEventListener("mouseout", (e) => {
+    for (let card of places) {
+      card.classList.remove("help");
+      let x1 = document.querySelector(".x");
+      let y1 = document.querySelector(".y");
 
-let stons = document.querySelectorAll(".stone");
-
-stons.forEach((item) => {
-  item.addEventListener("click", () => {
-    stons.forEach(element => {
-      element.classList.remove('ston_selected')
-    })
-    item.classList.add('ston_selected')
+      x1.innerHTML = "";
+      y1.innerHTML = "";
+    }
   });
-});
+}
